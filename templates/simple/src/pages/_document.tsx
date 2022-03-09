d7d7fd13940c064fad28d7dae0524ead023da6d7
Mike Barkmin <mike@barkmin.eu>
2022-03-09 14:15:28 +0100

    use cdn for katex and hljs

--- templates/simple/src/pages/_document.tsx
+++ templates/simple/src/pages/_document.tsx
@@ -4,8 +4,16 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="stylesheet" href="/katex/katex.min.css" />
-        <link rel="stylesheet" href="/hljs/hljs.min.css" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/katex@0.15.2/dist/katex.min.css"
+          integrity="sha384-MlJdn/WNKDGXveldHDdyRP1R4CTHr3FeuDNfhsLPYrq2t0UBkUdK2jyTnXPEK1NQ"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="stylesheet"
+          href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/styles/default.min.css"
+        />
       </Head>
       <body>
         <Main />
