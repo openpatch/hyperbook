84bba40a449b01bffe5ec86a7fe484cbd4a064e0
Mike Barkmin <mike@barkmin.eu>
2022-10-04 10:53:06 +0200

    Fix #294

---
+++ .changeset/new-suns-invent.md
@@ -0,0 +1,5 @@
+---
+"hyperbook": patch
+---
+
+Fix bug requiring admin permissions for creating symlinks #294
--- packages/hyperbook/helpers/make-symlink.ts
+++ packages/hyperbook/helpers/make-symlink.ts
@@ -1,8 +1,10 @@
 import fs from "fs";
+import os from "os";
 
 export async function makeSymlink(src: string, dst: string): Promise<void> {
+  const type = os.platform() == "win32" ? "junction" : null;
   return new Promise((resolve, reject) => {
-    fs.symlink(src, dst, (err) => {
+    fs.symlink(src, dst, type, (err) => {
       if (err) {
         reject();
       } else {
