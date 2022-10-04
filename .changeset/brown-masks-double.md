a7be20c426ee96cefdc198e843b30eda32e96c75
Mike Barkmin <mike@barkmin.eu>
2022-10-05 00:11:04 +0200

    remove react-icons

---
+++ .changeset/brown-masks-double.md
@@ -0,0 +1,5 @@
+---
+"@hyperbook/markdown": patch
+---
+
+remove react-icons
--- packages/markdown/src/Code.tsx
+++ packages/markdown/src/Code.tsx
@@ -1,7 +1,40 @@
 import { useDirectives } from "@hyperbook/provider";
-import React, { Fragment, useRef, useState } from "react";
+import { Fragment, useRef, useState } from "react";
 import { Components } from "react-markdown";
-import { MdContentCopy, MdDone } from "react-icons/md";
+
+const MdContentCopy = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      stroke="currentColor"
+      fill="currentColor"
+      strokeWidth="0"
+      height="1em"
+      width="1em"
+      viewBox="0 0 24 24"
+    >
+      <path d="M0 0h24v24H0z" fill="none" />
+      <path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z" />
+    </svg>
+  );
+};
+
+const MdDone = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      stroke="currentColor"
+      fill="currentColor"
+      strokeWidth="0"
+      height="1em"
+      width="1em"
+      viewBox="0 0 24 24"
+    >
+      <path d="M0 0h24v24H0z" fill="none" />
+      <path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z" />
+    </svg>
+  );
+};
 
 export const Code: Components["code"] = ({ children, className }) => {
   const directives = useDirectives();
