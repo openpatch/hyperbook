2574ee6f0f4c70b1f05a70bbd8b1d5ba0eba013f
Mike Barkmin <mike@barkmin.eu>
2022-10-13 23:03:51 +0200

    Allow to reveal protect in dev mode
    Fixes #361

---
+++ .changeset/clever-pens-return.md
@@ -0,0 +1,5 @@
+---
+"@hyperbook/element-protect": minor
+---
+
+In development mode you can now use the reveal button to reveal the elements under the protect element. This should ease the development. This does not have any effects on the production side.
--- packages/element-protect/src/index.tsx
+++ packages/element-protect/src/index.tsx
@@ -1,8 +1,8 @@
-import { FC, Fragment, ReactNode } from "react";
+import { FC, Fragment, ReactNode, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { createSlice, PayloadAction } from "@hyperbook/store";
 import hash from "object-hash";
-import { useActivePageId } from "@hyperbook/provider";
+import { useActivePageId, useEnv } from "@hyperbook/provider";
 import "./index.css";
 
 type DirectiveProtectProps = {
@@ -20,25 +20,45 @@ const DirectiveProtect: FC<DirectiveProtectProps> = ({
 }) => {
   const [activePageId] = useActivePageId();
   const dispatch = useDispatch();
+  const env = useEnv();
   const id = `${activePageId}.${hash(node)}`;
   const value = useSelector(selectValue(id));
+  const [reveal, setReveal] = useState(false);
 
   const setValue = (value: string) => {
     dispatch(sliceProtect.actions.set({ id, value }));
   };
 
-  return value === password ? (
-    <>{children}</>
-  ) : (
-    <div className="element-protect">
-      <span className="description">{description}</span>
-      <span className="icon">🔒</span>
-      <input
-        placeholder="..."
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-      />
-    </div>
+  useEffect(() => {
+    if (value === password) {
+      setReveal(true);
+    }
+  }, [value]);
+
+  return (
+    <>
+      {reveal ? (
+        <>{children}</>
+      ) : (
+        <div className="element-protect">
+          <span className="description">{description}</span>
+          <span className="icon">🔒</span>
+          <input
+            placeholder="..."
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+        </div>
+      )}
+
+      {env === "development" && (
+        <div className="element-protect-dev">
+          <button className="reveal" onClick={() => setReveal((r) => !r)}>
+            Toggle Reveal
+          </button>
+        </div>
+      )}
+    </>
   );
 };
 
