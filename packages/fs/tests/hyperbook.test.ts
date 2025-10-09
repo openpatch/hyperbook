07c5907f17fa022d05addbef243cfd5d1b3787fd
Mike Barkmin <mike@barkmin.eu>
2025-10-09 22:19:58 +0200

    use relative paths in test

--- packages/fs/tests/hyperbook.test.ts
+++ packages/fs/tests/hyperbook.test.ts
@@ -1,34 +1,20 @@
 import path from "path";
 import { describe, it, expect } from "vitest";
 import { hyperbook, vfile } from "../src";
+import { HyperbookPage } from "@hyperbook/types/dist";
 
 describe("hyperbook", () => {
   const relative = (s: string) =>
     path.relative(path.join(__dirname, "fixtures"), s);
-  const makeFileRelative = (f: vfile.VFile) => {
-    if ("references" in f) {
-      return {
-        ...f,
-        root: relative(f.root),
-        path: { ...f.path, absolute: relative(f.path.absolute) },
-        elements: [],
-        references: f.references.map((f) => ({
-          ...f,
-          root: relative(f.root),
-          path: { ...f.path, absolute: relative(f.path.absolute) },
-        })),
-      } as vfile.VFileGlossary;
-    }
+  const makeFileRelative = (p: HyperbookPage) => {
     return {
-      ...f,
-      root: relative(f.root),
-      path: { ...f.path, absolute: relative(f.path.absolute) },
-    };
+      ...p,
+      path: { ...p.path, absolute: relative(p.path?.absolute || "") },
+    } as HyperbookPage;
   };
   it("should get navigation", async () => {
     let hyperbookPath = path.join(__dirname, "fixtures", "single-hyperbook");
     let files = await vfile.list(hyperbookPath);
-    files = files.map(makeFileRelative);
     let current = files.find(
       (f) => f.name === "paradigms" && f.folder === "book",
     );
@@ -40,7 +26,10 @@ describe("hyperbook", () => {
       pagesAndSections.sections,
       pagesAndSections.pages,
     );
-    const navigation = await hyperbook.getNavigationForFile(pageList, current);
+    const navigation = await hyperbook.getNavigationForFile(
+      pageList.map(makeFileRelative),
+      current,
+    );
     expect(navigation).toMatchSnapshot();
   });
 });
