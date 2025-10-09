44cfefee161bf2d0e9a2578f4df07d4e63bef418
Mike Barkmin <mike@barkmin.eu>
2025-10-09 22:15:57 +0200

    use relative paths in text

--- packages/fs/tests/hyperbook.test.ts
+++ packages/fs/tests/hyperbook.test.ts
@@ -3,9 +3,32 @@ import { describe, it, expect } from "vitest";
 import { hyperbook, vfile } from "../src";
 
 describe("hyperbook", () => {
+  const relative = (s: string) =>
+    path.relative(path.join(__dirname, "fixtures"), s);
+  const makeFileRelative = (f: vfile.VFile) => {
+    if ("references" in f) {
+      return {
+        ...f,
+        root: relative(f.root),
+        path: { ...f.path, absolute: relative(f.path.absolute) },
+        elements: [],
+        references: f.references.map((f) => ({
+          ...f,
+          root: relative(f.root),
+          path: { ...f.path, absolute: relative(f.path.absolute) },
+        })),
+      } as vfile.VFileGlossary;
+    }
+    return {
+      ...f,
+      root: relative(f.root),
+      path: { ...f.path, absolute: relative(f.path.absolute) },
+    };
+  };
   it("should get navigation", async () => {
     let hyperbookPath = path.join(__dirname, "fixtures", "single-hyperbook");
     let files = await vfile.list(hyperbookPath);
+    files = files.map(makeFileRelative);
     let current = files.find(
       (f) => f.name === "paradigms" && f.folder === "book",
     );
